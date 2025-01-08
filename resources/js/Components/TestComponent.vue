22821a6e65a52baf01ec5fc830e092bd7523b70c
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2025-01-08 20:06:12 -0300

    Initial commit

---
+++ resources/js/Components/TestComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1 class="text-2xl text-blue-500">Hello from Vue.js</h1>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "TestComponent",
+  };
+  </script>
+  <style scoped></style>
+  
