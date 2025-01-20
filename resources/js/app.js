db5f9671e118af43dfd3a9ac7a905bb35d32c314
Matheus Pimentel <62631247+MaPimentell@users.noreply.github.com>
2025-01-20 12:45:29 -0300

    remove-jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import $ from 'jquery';
-window.$ = $;
 import TestComponent from './Components/TestComponent.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
